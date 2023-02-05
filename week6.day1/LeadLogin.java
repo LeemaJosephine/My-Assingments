ca89f6f33e4623cce80e32ec26891675e0a6c10c
LeemaJosephine <121459183+LeemaJosephine@users.noreply.github.com>
2023-02-05 13:12:00 +0530

    Add files via upload

---
+++ week6.day1/LeadLogin.java
@@ -0,0 +1,47 @@
+package week6.day1;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LeadLogin {
+
+	public ChromeDriver driver;
+	
+	@BeforeClass
+	public void details() {
+		System.out.println("Author Name: Leema Josephine");
+	}
+	@BeforeMethod
+	public void login() {
+		
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://leaftaps.com/opentaps/");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.findElement(By.id("username")).sendKeys("DemoCSR");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.linkText("CRM/SFA")).click();
+		driver.findElement(By.linkText("Leads")).click();
+	}
+	
+	@AfterMethod
+	public void closeBrowser() {
+		driver.close();
+	}
+	
+	@AfterClass
+	public void closeInformation() {
+		System.out.println("Execution done!!");
+	}
+
+}
